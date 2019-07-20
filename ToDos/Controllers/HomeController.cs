a895ae5e6dce148129d76cb1ff0a0d4d52c10a9d
Tahreem Iqbal <tahreem.iqbal23@gmail.com>
2019-07-20 19:53:51 +0500

    Add project files.

---
+++ ToDos/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDos.Models;
+
+namespace ToDos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.lstToDos = TodoData.lstToDos;
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(ToDo model)
+        {
+            int maxId = 1;
+
+            if (TodoData.lstToDos.Count > 0)
+                maxId = TodoData.lstToDos.Max(x => x.id) + 1;
+
+            TodoData.lstToDos.Add(new ToDo() { id = maxId, todo = model.todo, isComplete = false });
+
+            ViewBag.lstToDos = TodoData.lstToDos;
+            return View("Index");
+        }
+
+        public IActionResult DeleteToDo(int id)
+        {
+            TodoData.lstToDos.RemoveAll(x => x.id == id);
+
+            ViewBag.lstToDos = TodoData.lstToDos;
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult changeToDoStatus(int id, bool isComplete)
+        {
+            var obj = TodoData.lstToDos.FirstOrDefault(x => x.id == id);
+            if (obj != null) obj.isComplete = isComplete;
+
+            ViewBag.lstToDos = TodoData.lstToDos;
+            return View("Index");
+        }
+    }
+}
---
+++ ToDos/Models/ToDo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDos.Models
+{
+    public class ToDo
+    {
+        public int id { get; set; }
+        public string todo { get; set; }
+        public bool isComplete { get; set; }
+    }
+
+    public class TodoData
+    {
+        public static List<ToDo> lstToDos = new List<ToDo>()
+        {
+            new ToDo() { id = 1, todo = "get groceries", isComplete = false },
+            new ToDo() { id = 2, todo = "go shopping", isComplete = false },
+            new ToDo() { id = 3, todo = "write report", isComplete = false },
+            new ToDo() { id = 4, todo = "do laundary", isComplete = false },
+            new ToDo() { id = 5, todo = "do homework", isComplete = false }
+        };
+    }
+}
